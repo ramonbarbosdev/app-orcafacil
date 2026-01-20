d6f328eb2dc723d4a8721c424264e063b1a894c1
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2026-01-20 13:51:33 -0300

    feat: docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-agrotech.ramoncode.com.br/agrotech',
-  apiUrlWebSocket:'https://api-agrotech.ramoncode.com.br/agrotech/agrotech-socket',
+  apiUrl: 'https://api-orcafacil.ramoncode.com.br/orcafacil',
+  apiUrlWebSocket:'https://api-orcafacil.ramoncode.com.br/orcafacil/orcafacil-socket',
 };
